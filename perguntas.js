833c8b90f66fca41c1c4f1ebdc3e47195df626e3
MiguelDuda <migueldudadossantosneto@gmail.com>
2024-12-02 07:40:25 -0300

    Miguel Alura

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Corintians',
+    'Em que ano o Corinthians foi fundado?',
+    'O Corinthians foi fundadao no ano de 1910'
+)
+
+criaCartao(
+    'Corinthians',
+    'Quantos Mundiais de Clubes o Corinthians tem?',
+    'O Corinthians já conquistou o Mundial de Clubes duas vezes'
+)
+
+criaCartao(
+    'Corinthians',
+    'Quais foram os anos que o Cornthians ganhou seus Mundiais de Clubes?',
+    'O Corinthians ganhou seus Mundiais de clubes em 2000 e 2012 '
+)
+
+criaCartao(
+    'Corinthians',
+    'Quantos Estaduais o Corinthians tem?',
+    'O Corinthians tem 30 Estaduais'
+)
